d265da8b5d344a66f1df142178869e630a29f218
ywcheong <personal@ywcheong.com>
2026-03-02 23:04:25 +0900

    init: 프로젝트 생성

---
+++ src/main/kotlin/ywcheong/sofia/SofiaApplication.kt
@@ -0,0 +1,11 @@
+package ywcheong.sofia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SofiaApplication
+
+fun main(args: Array<String>) {
+    runApplication<SofiaApplication>(*args)
+}
---
+++ src/test/kotlin/ywcheong/sofia/SofiaApplicationTests.kt
@@ -0,0 +1,13 @@
+package ywcheong.sofia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SofiaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
